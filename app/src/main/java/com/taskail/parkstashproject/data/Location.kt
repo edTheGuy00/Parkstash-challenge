e2dec73bb668d9492bec46cbf4604787c5602dbf
edgar <edgrr.trem000@gmail.com>
2018-04-11 13:42:40 -0500

    fix build errors

--- app/src/main/java/com/taskail/parkstashproject/data/Location.kt
+++ app/src/main/java/com/taskail/parkstashproject/data/Location.kt
@@ -1,6 +1,5 @@
 package com.taskail.parkstashproject.data
 
-import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import java.util.*
@@ -10,9 +9,9 @@ import java.util.*
  */
 
 @Entity(tableName = "locations")
-data class Location @JvmOverloads constructor(
-        @ColumnInfo(name = "loccation_title") var locationTitle: String = "",
-        @ColumnInfo(name = "location_lat") var locationLat: Double,
-        @ColumnInfo(name = "location_lng") var locationLng: Double,
-        @PrimaryKey @ColumnInfo(name = "entry_id") var id: String = UUID.randomUUID().toString()
+data class Location(
+        var locationTitle: String,
+        var locationLat: Double,
+        var locationLng: Double,
+        @PrimaryKey var id: String = UUID.randomUUID().toString()
 )
